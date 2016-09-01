8df475772eea256f7ab88fc9caf06c5be403ba4c
Sebastian YEPES F <syepes@gmail.com>
2016-09-01 13:52:54 +0200

    Refactor exportConfig + git support

--- zbx2git.rb
+++ zbx2git.rb
@@ -1,113 +1,146 @@
+require 'logger'
+require 'fileutils'
 require "zabbixapi"
+require 'git'
 
-def exportTemplates(zbx)
-   templates = zbx.query(
-       :method => "template.get",
-       :params => {:output => ["templateid","name"], :sortorder => "templateid"}
-   )
 
-   for template in templates do
-      #puts JSON.pretty_generate(template)
-      printf "Filename : '%s/%s'\n", Dir.pwd(), template["name"]+".json"
+logger = Logger.new('test.log', 'weekly')
+logger.level = Logger::WARN
 
-      json = JSON.parse(zbx.query(
+def secs2human(secs)
+  [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{|count, name|
+    if secs > 0
+      secs, n = secs.divmod(count)
+      "#{n.to_i} #{name}"
+    end
+  }.compact.reverse.join(' ')
+end
+
+def exportConfig(inst, zbx, cfg)
+  begin
+    ts_s = Time.now.to_i
+    puts "> Start Exporting: #{cfg[:type]} (#{inst})"
+
+    results = zbx.query(
+      :method => cfg[:method],
+      :params => {:output => cfg[:output], :sortorder => cfg[:sortorder]}
+    )
+
+    path = Dir.pwd()+"/#{inst}/#{cfg[:type]}"
+    for result in results do
+      begin
+        #puts JSON.pretty_generate(result)
+        # Fime name normalization
+        file = result["name"]+".json".gsub(/[^a-zA-Z0-9\-_\s\.\(\)]/,"")
+        #printf " '%s/%s'\n", path, file
+
+        json = JSON.parse(zbx.query(
           :method => "configuration.export",
           :params => {
             :options => {
-              :templates => [ template["templateid"] ],
+              cfg[:type] => [ result[cfg[:id]] ],
             },
             :format => 'json'
           }
-      ))
+        ))
 
-      json_pretty = JSON.pretty_generate(json)
-      Dir.mkdir(Dir.pwd()+"/templates") unless File.exists?(Dir.pwd()+"/templates")
-      File.open("templates/"+template["name"]+".json","w"){|file| file.puts json_pretty}
-   end
+        # Clear export data to prevent unnecessary changes
+        json["zabbix_export"]["date"] = ""
+        json_pretty = JSON.pretty_generate(json)
+
+        FileUtils.mkdir_p(path) unless File.exists?(path)
+        File.open("#{path}/#{file}","w"){|f| f.puts json_pretty}
+      rescue Exception => e
+        logger.error "Error Exporting: #{cfg[:type]} (#{inst}) : #{e.message}"
+      end
+    end
+
+    begin
+      if !File.exists?(path+"/.git")
+        g = Git.init(path)
+        g.add(:all=>true)
+        m = g.commit_all('Initial')
+        puts "> Git (Init): #{m}" if m != nil
+      else
+        g = Git.open(path)
+        g.add(:all=>true)
+        m = g.commit_all('Change') if !g.status.changed.empty?
+        puts "> Git (Change): #{m}" if m != nil
+      end
+    rescue Exception => e
+       logger.error "Error saving changes to git: #{cfg[:type]} (#{inst}) : #{e.message}"
+    end
+
+    puts "> Finished Exporting: #{cfg[:type]} (#{inst}) in #{secs2human(Time.now.to_i - ts_s)}"
+  rescue Exception => e
+    logger.error "Error Exporting: #{cfg[:type]} (#{inst}) : #{e.message} : #{e.backtrace.inspect}"
+  end
 end
 
-def exportValueMaps(zbx)
-   valuemaps = zbx.query(
-       :method => "valuemap.get",
-       :params => {:output => ["valuemapid","name"], :sortorder => "valuemapid"}
-   )
 
-   for valuemap in valuemaps do
-      #puts JSON.pretty_generate(valuemap)
-      printf "Filename : '%s/%s'\n", Dir.pwd(), valuemap["name"]+".json"
+@export_cfg = [{ :type => :hosts,
+                 :method => "host.get",
+                 :output => ["hostid","name"],
+                 :sortorder => "hostid",
+                 :id => "hostid"
+               },
+               { :type => :groups,
+                 :method => "hostgroup.get",
+                 :output => ["groupid","name"],
+                 :sortorder => "groupid",
+                 :id => "groupid"
+               },
+               { :type => :valueMaps,
+                 :method => "valuemap.get",
+                 :output => ["valuemapid","name"],
+                 :sortorder => "valuemapid",
+                 :id => "valuemapid"
+               },
+               { :type => :templates,
+                 :method => "template.get",
+                 :output => ["templateid","name"],
+                 :sortorder => "templateid",
+                 :id => "templateid"
+               },
+               { :type => :images,
+                 :method => "image.get",
+                 :output => ["imageids","name"],
+                 :sortorder => "imageids",
+                 :id => "imageids"
+               },
+               { :type => :maps,
+                 :method => "map.get",
+                 :output => ["sysmapids","name"],
+                 :sortorder => "sysmapids",
+                 :id => "sysmapids"
+               },
+               { :type => :screens,
+                 :method => "screen.get",
+                 :output => ["screenids","name"],
+                 :sortorder => "screenids",
+                 :id => "screenids"
+               }
+              ]
 
-      json = JSON.parse(zbx.query(
-          :method => "configuration.export",
-          :params => {
-            :options => {
-              :valueMaps => [ valuemap["valuemapid"] ],
-            },
-            :format => 'json'
-          }
-      ))
+@zabbix_cfg = [{ :inst => "PDC-LAB",
+                 :url => "http://localhost/api_jsonrpc.php",
+                 :user => "Admin",
+                 :password => "zabbix",
+               }]
 
-      json_pretty = JSON.pretty_generate(json)
-      #File.open(valuemap["name"]+".json","w"){|file| file.puts json_pretty}
-   end
+for zab_cfg in @zabbix_cfg do
+  begin
+     ts_s = Time.now.to_i
+     puts "> Start Collecting: #{zab_cfg[:inst]}"
+     zbx = ZabbixApi.connect(:url => zab_cfg[:url], :user => zab_cfg[:user], :password => zab_cfg[:password])
+
+     for exp_cfg in @export_cfg do
+       exportConfig(zab_cfg[:inst], zbx, exp_cfg)
+     end
+     puts "> Finished Collecting: #{zab_cfg[:inst]} in #{secs2human(Time.now.to_i - ts_s)}"
+
+  rescue Exception => e
+    logger.error "Error Connecting to Zabbix: #{zab_cfg[:inst]} : #{e.message}"
+  end
 end
 
-def exportGroups(zbx)
-   groups = zbx.query(
-       :method => "hostgroup.get",
-       :params => {:output => ["groupid","name"], :sortorder => "groupid"}
-   )
-
-   for group in groups do
-      #puts JSON.pretty_generate(group)
-      printf "Filename : '%s/%s'\n", Dir.pwd(), group["name"]+".json"
-
-      json = JSON.parse(zbx.query(
-          :method => "configuration.export",
-          :params => {
-            :options => {
-              :groups => [ group["groupid"] ],
-            },
-            :format => 'json'
-          }
-      ))
-
-      json_pretty = JSON.pretty_generate(json)
-      File.open(group["name"]+".json","w"){|file| file.puts json_pretty}
-   end
-end
-
-def exportHosts(zbx)
-   hosts = zbx.query(
-       :method => "host.get",
-       :params => {:output => ["hostid","name"], :sortorder => "hostid"}
-   )
-
-   for host in hosts do
-      #puts JSON.pretty_generate(host)
-      printf "Filename : '%s/%s'\n", Dir.pwd(), host["name"]+".json"
-
-      json = JSON.parse(zbx.query(
-          :method => "configuration.export",
-          :params => {
-            :options => {
-              :hosts => [ host["hostid"] ],
-            },
-            :format => 'json'
-          }
-      ))
-
-      json_pretty = JSON.pretty_generate(json)
-      File.open(host["name"]+".json","w"){|file| file.puts json_pretty}
-   end
-end
-
-zbx = ZabbixApi.connect(
-  :url => 'http://localhost/api_jsonrpc.php',
-  :user => 'Admin',
-  :password => 'zabbix'
-)
-
-exportTemplates(zbx)
-exportValueMaps(zbx)
-exportGroups(zbx)
-exportHosts(zbx)
