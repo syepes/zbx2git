f93d1ecf66357fed471c82d46d1284e16e61a530
Sebastian YEPES F <syepes@gmail.com>
2016-09-06 15:46:09 +0200

    handle deletes and logging scope

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ This CHANGELOG follows the format listed at [Keep A Changelog](http://keepachang
 
 ## [Unreleased]
 
+## 0.1.1 - 2016-09-06
+### Fixed
+- Correctly handle deleted files
+- Fix logging variable scope and logging levels
+
 ## 0.1.0 - 2016-09-02
 ### Added
 - Initial release
--- zbx2git.rb
+++ zbx2git.rb
@@ -28,14 +28,17 @@ end
 def exportConfig(logger, inst, zbx, cfg)
   begin
     ts_s = Time.now.to_i
-    logger.warn "Start Exporting: #{cfg[:type]} (#{inst})"
+    path = "#{Dir.pwd()}/#{inst}/#{cfg[:type]}"
+    logger.info "Start Exporting: #{cfg[:type]} (#{inst})"
 
     results = zbx.query(
       :method => cfg[:method],
       :params => {:output => cfg[:output], :sortorder => cfg[:sortorder]}
     )
 
-    path = "#{Dir.pwd()}/#{inst}/#{cfg[:type]}"
+    # Clean previously exported files
+    FileUtils.rm_f(Dir.glob("#{path}/*"))
+
     for result in results do
       begin
         # File name normalization
@@ -68,20 +71,26 @@ def exportConfig(logger, inst, zbx, cfg)
       if !File.exists?("#{path}/.git")
         g = Git.init(path, :log => logger)
         g.add(:all=>true)
-        m = g.commit_all('Initial')
-        logger.info "Git (Init): #{m}" if m != nil
+        m = g.commit_all('initial')
+        logger.debug "Git (Init): #{m}" if m != nil
         File.open("#{path}/.git/description","w"){|f| f.puts "#{inst} - #{cfg[:type]}"}
       else
         g = Git.open(path, :log => $logger)
         g.add(:all=>true)
-        m = g.commit_all('Change') if !g.status.changed.empty?
-        logger.info "Git (Change): #{m}" if m != nil
+
+        any_change = ["changed","added","deleted"].map{|x| x if !g.status.send(x.to_sym).empty?}.compact.delete_if(&:empty?)
+        if !any_change.empty?
+          logger.warn "Detected changes: (#{any_change.join(', ')}) in #{cfg[:type]} (#{inst})"
+        end
+
+        m = g.commit_all("#{any_change.join(', ')}") if !any_change.empty?
+        logger.debug "Git (#{any_change.join(', ')}): #{m}" if m != nil
       end
     rescue Exception => e
        logger.error "Saving changes to git: #{cfg[:type]} (#{inst}) : #{e.message}"
     end
 
-    logger.warn "Finished Exporting: #{cfg[:type]} (#{inst}) in #{secs2human(Time.now.to_i - ts_s)}"
+    logger.info "Finished Exporting: #{cfg[:type]} (#{inst}) in #{secs2human(Time.now.to_i - ts_s)}"
   rescue Exception => e
     logger.error "Exporting: #{cfg[:type]} (#{inst}) : #{e.message}"
     logger.debug "Trace: #{e.backtrace.inspect}"
@@ -91,39 +100,39 @@ end
 
 
 
-logger = Logger.new("zbx2git.log", 'monthly')
-logger.level = Logger::WARN
+log = Logger.new("zbx2git.log", 'monthly')
+log.level = Logger::INFO
 
 begin
   cfg = JSON.parse(File.read('zbx2git.json'), :symbolize_names => true)
 rescue Exception => e
-  logger.warn "Error Loading config file: zbx2git.json : #{e.message}"
-  logger.debug "Trace: #{e.backtrace.inspect}"
+  log.error "Error Loading config file: zbx2git.json : #{e.message}"
+  log.debug "Trace: #{e.backtrace.inspect}"
   exit 1
 end
 
 
-logger.warn "Start Collecting of instances: #{cfg[:zabbix_cfg].map{|i| i[:inst] }.join(', ')}"
+log.info "Start Collecting of instances: #{cfg[:zabbix_cfg].map{|i| i[:inst] }.join(', ')}"
 
 ts_s = Time.now.to_i
 Parallel.each(cfg[:zabbix_cfg], in_threads: 5) { |zab_cfg|
   logger = Logger.new("zbx2git_#{zab_cfg[:inst]}.log", 'monthly')
-  logger.level = Logger::WARN
+  logger.level = Logger::INFO
 
   begin
     ts_s = Time.now.to_i
-    logger.warn "Start Collecting: #{zab_cfg[:inst]} - #{zab_cfg[:url]}"
+    logger.info "Start Collecting: #{zab_cfg[:inst]} - #{zab_cfg[:url]}"
     zbx = ZabbixApi.connect(:url => zab_cfg[:url], :user => zab_cfg[:user], :password => zab_cfg[:password], :timeout => 15)
 
     for exp_cfg in cfg[:export_cfg] do
       exportConfig(logger, zab_cfg[:inst], zbx, exp_cfg)
     end
-    logger.warn "Finished Collecting: #{zab_cfg[:inst]} in #{secs2human(Time.now.to_i - ts_s)}"
+    logger.info "Finished Collecting: #{zab_cfg[:inst]} in #{secs2human(Time.now.to_i - ts_s)}"
 
   rescue Exception => e
     logger.error "Connecting to Zabbix: #{zab_cfg[:inst]} : #{e.message}"
     logger.debug "Trace: #{e.backtrace.inspect}"
   end
 }
-logger.warn "Completed in #{secs2human(Time.now.to_i - ts_s)}"
+log.info "Completed in #{secs2human(Time.now.to_i - ts_s)}"
 
